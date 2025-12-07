f76600ff4c29401da2372cea4e8ea3102ecb192f
maen0002 <maen0002@stud.kea.dk>
2025-12-07 20:46:15 +0100

    feat: implement comment functionality with form and submission handling

--- components/shared/button/Button.tsx
+++ components/shared/button/Button.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import * as motion from "motion/react-client";
 
 interface ButtonProps {
   text?: string;
@@ -10,12 +11,40 @@ interface ButtonProps {
 const Button = ({ text, isLink = false, route = "#", stylePlace }: ButtonProps) => {
   if (isLink) {
     return (
-      <Link href={route} className={`border-y-2 border-white px-8 uppercase py-4 ${stylePlace}`}>
-        {text}
+      <Link className={stylePlace} href={route}>
+        <motion.p
+          whileHover={{
+            color: "var(--color-primary)",
+            borderColor: "var(--color-primary)",
+            borderInlineWidth: "3px",
+            // Will be used when gesture starts
+            transition: { duration: 0.3 },
+          }}
+          // Will be used when gesture ends
+          transition={{ duration: 0.5 }}
+          className={`inline-block text-center border-y-2 border-white min-w-[180px] uppercase py-4`}
+        >
+          {text}
+        </motion.p>
       </Link>
     );
   } else {
-    return <button className="border-y-2 border-white px-8 uppercase py-4">{text}</button>;
+    return (
+      <motion.button
+        whileHover={{
+          color: "var(--color-primary)",
+          borderColor: "var(--color-primary)",
+          borderInlineWidth: "3px",
+          // Will be used when gesture starts
+          transition: { duration: 0.3 },
+        }}
+        // Will be used when gesture ends
+        transition={{ duration: 0.5 }}
+        className={`border-y-2 border-white min-w-[180px] uppercase py-4 cursor-pointer ${stylePlace}`}
+      >
+        {text}
+      </motion.button>
+    );
   }
 };
 
