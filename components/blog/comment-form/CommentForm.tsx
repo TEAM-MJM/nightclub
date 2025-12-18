dcbf4c02b867d1057b753e306a7c57d82fd88f62
maen0002 <maen0002@stud.kea.dk>
2025-12-18 17:03:50 +0100

    Fixed Comment error handling and added effects using motion and isSelected prop

--- components/blog/comment-form/CommentForm.tsx
+++ components/blog/comment-form/CommentForm.tsx
@@ -27,15 +27,15 @@ const CommentForm = ({ id, onSuccess }: CommentFormProps) => {
         <input type="hidden" name="blogId" value={id}></input>
         <div>
           {state.error?.yourName && <p className="text-red-500">{state.error.yourName}</p>}
-          <input type="text" name="yourName" placeholder="Your Name" className="border w-full py-6 px-3"></input>
+          <input type="text" name="yourName" placeholder="Your Name" defaultValue={!state.success ? state.yourName : ""} className="border w-full py-6 px-3"></input>
         </div>
         <div>
           {state.error?.yourEmail && <p className="text-red-500">{state.error.yourEmail}</p>}
-          <input type="email" name="yourEmail" placeholder="Your Email" className="border px-3 w-full py-6"></input>
+          <input type="email" name="yourEmail" placeholder="Your Email" defaultValue={!state.success ? state.yourEmail : ""} className="border px-3 w-full py-6"></input>
         </div>
         <div className="col-span-2">
           {state.error?.yourComment && <p className="text-red-500 col-start-1 row-start-1">{state.error.yourComment}</p>}
-          <textarea name="yourComment" rows={10} placeholder="Your Comment" className="border px-3 py-5 w-full"></textarea>
+          <textarea name="yourComment" rows={10} placeholder="Your Comment" defaultValue={!state.success ? state.yourComment : ""} className="border px-3 py-5 w-full"></textarea>
         </div>
         <Button isLink={false} text="Submit" stylePlace="place-self-end col-start-2"></Button>
       </form>
